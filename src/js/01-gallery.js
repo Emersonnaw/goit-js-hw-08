76f7d389e06ea8a1046dc95b08472138b94b4ab0
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-03-05 23:32:21 +0200

    solved the task one

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('ul.gallery');
+
+function createMarkupGallery(gallery){
+    return gallery.map(({preview, original, description }) => {
+       return `
+       <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+    ` 
+    }).join('');
+    //return markup;
+    }
+
+const markup = createMarkupGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', linkBan);
+
+function linkBan (e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG"){
+     return;
+  }
+ }
+
+ var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay:250});
 console.log(galleryItems);
