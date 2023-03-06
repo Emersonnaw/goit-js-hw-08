eedde5f09384fd984947dad00a44546a01084fad
Vladimir <86652590+Emersonnaw@users.noreply.github.com>
2023-03-06 03:21:08 +0200

    Assignment 2 was solved

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+import Player  from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+    console.log(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+};
+player.on('timeupdate', throttle(onPlay,1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
